fd407f93bd97b7fd0ad2b0427133f507d9c27fc9
Mickey <hedgefund996@gmail.com>
2022-04-22 19:16:34 +0800

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// fn declare function, like def in ts/python
+fn main() {
+    // while the ! in println! is a macro：https://doc.rust-lang.org/book/ch19-06-macros.html
+    println!("Hello, {} world! ", 3);
+}
