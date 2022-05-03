56405fc693f4ffa96f4b468db3159cc2d9972333
Mickey <hedgefund996@gmail.com>
2022-05-03 16:50:09 +0800

    cargo run example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-// fn declare function, like def in ts/python
-fn main() {
-    // while the ! in println! is a macro：https://doc.rust-lang.org/book/ch19-06-macros.html
-    let action = std::env::args().nth(1).expect("Please specify an action");
-    let item = std::env::args().nth(2).expect("Please specify an item");
+// // fn declare function, like def in ts/python
+// fn main() {
+//     // while the ! in println! is a macro：https://doc.rust-lang.org/book/ch19-06-macros.html
+//     let action = std::env::args().nth(1).expect("Please specify an action");
+//     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?}, {:?}", action, item);
-}
+//     println!("{:?}, {:?}", action, item);
+// }
---
+++ src/output.rs
@@ -0,0 +1,8 @@
+// fn declare function, like def in ts/python
+fn main() {
+  // while the ! in println! is a macro：https://doc.rust-lang.org/book/ch19-06-macros.html
+  let action = std::env::args().nth(1).expect("Please specify an action");
+  let item = std::env::args().nth(2).expect("Please specify an item");
+
+  println!("{:?}, {:?}", action, item);
+}
